c911419b33dee91cb95d0fca62d1e691218447d7
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-07-10 22:24:40 +0530

    changes

--- reactRouter/src/App.jsx
+++ reactRouter/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +11,8 @@ function App() {
   return (
     <>
       <h1 className='bg-green-600 p-4'></h1>
+      <Header/>
+      <Footer/>
     </>
   )
 }
