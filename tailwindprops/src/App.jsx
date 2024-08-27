b830328befe82cb7827f23594cc5e56c6d5649a5
Vidhi1010 <vidhisinghvp1010@gmail.com>
2024-08-27 15:47:45 +0530

    new

--- tailwindprops/src/App.jsx
+++ tailwindprops/src/App.jsx
@@ -10,11 +10,11 @@ function App() {
       <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
         Tailwind test
       </h1>
-      <Card name = {"Vidhi"} btn = {"Click Me"}/>
-      <Card name = {"Vivek"}/>
-      <Card name = {"Aanchal"} />
+      <Card name = {"Vidhi"} btntext = {"Click Me"}/>
+      <Card name = {"Vivek"} btntext = {"Click Me"}/>
+      <Card name = {"Vaibhav"} />
     </>
-  );
+  ); 
 }
 
 export default App;
